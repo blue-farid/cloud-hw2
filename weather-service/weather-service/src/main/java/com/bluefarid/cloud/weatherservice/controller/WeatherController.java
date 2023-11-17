5ca3acb270b5902393080cd342c4e9b029113991
blue_farid <blue_farid@hotmail.com>
2023-11-17 21:22:55 +0330

    fix

--- weather-service/weather-service/src/main/java/com/bluefarid/cloud/weatherservice/controller/WeatherController.java
+++ weather-service/weather-service/src/main/java/com/bluefarid/cloud/weatherservice/controller/WeatherController.java
@@ -3,6 +3,8 @@ package com.bluefarid.cloud.weatherservice.controller;
 import com.bluefarid.cloud.weatherservice.response.WeatherAPIResponse;
 import com.bluefarid.cloud.weatherservice.service.WeatherService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/weather")
 @RequiredArgsConstructor
+@Slf4j
 public class WeatherController {
     private final WeatherService service;
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
     @GetMapping
     public ResponseEntity<WeatherAPIResponse> checkWeather(String latAndLong) {
+        log.info("REDIS HOST: {}", redisHost);
         return ResponseEntity.ok(service.checkWeather(latAndLong));
     }
 
